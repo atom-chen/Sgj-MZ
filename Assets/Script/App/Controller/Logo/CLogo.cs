9267da43ac5bf6c4717f4ac256892d26a9e159bd
lufy <lufy.legend@gmail.com>
2018-10-25 18:59:27 +0900

    update

---
+++ Assets/Script/App/Controller/Logo/CLogo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using App.View.Common;
+using UnityEngine;
+namespace App.Controller.Common
+{
+    public class CLogo : CScene
+    {
+    }
+}
